35afb7e8463aeee6d129cf92f9a6aec8b15e4ead
Arman Shahariar <111569490+armanshahriar@users.noreply.github.com>
2023-02-25 09:56:30 +0600

    Version-1.0.0

---
+++ index.js
@@ -0,0 +1,42 @@
+// var index = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 22, 33, 44, 55, 66, 77, 88, 99, 00];
+// var element = index[2];
+// console.log(element);
+
+// index[10] = [10];
+// index[11] = [10];
+// console.log(index)
+
+// var positionIndex = index.indexOf(00)
+// console.log(positionIndex)
+
+// let myArray = [2, 3, 4, 5];
+// myArray.push(6);
+// myArray.pop()
+// myArray.unshift(1);
+// console.log(myArray); // Output: [1, 2, 3, 4, 5]
+
+// let mYArray = [1, 2, 3, 4, 5];
+// let removedElement = mYArray.shift();
+// console.log(mYArray); // Output: [2, 3, 4, 5]
+// // console.log(removedElement); // Output: 1
+// var ami = 85;
+// var tom =65;
+// var jane =95;
+// var petter =56;
+// var jhon =40;
+
+// if(80 <= ami ){
+//     console.log("pash");
+// }
+// var now = 1;
+// while (now <= 101) {
+//     console.log("now");
+//     now++;
+// };
+
+// var numbers = [12,23,45,6,7,76,545,66,44,768,86,346,452,346,]
+// for (var index = 0; index < numbers.length; index++) {
+//     var number = numbers[index];
+//     console.log(number)
+    
+// }
