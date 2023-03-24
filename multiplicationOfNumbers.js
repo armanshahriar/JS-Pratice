92f2f96ebb86a0ac069b29501567d8a2521960a7
Arman Shahariar <111569490+armanshahriar@users.noreply.github.com>
2023-03-24 16:46:00 +0600

    version - 1.1.3

---
+++ multiplicationOfNumbers.js
@@ -0,0 +1,10 @@
+function multiplicationOfNumbers(number) {
+    let = multi = 1 ;
+    for (let i = 1; i <= number; i++) {
+        multi = multi * i;
+        console.log(i, multi)
+    }
+    return multi ;
+}
+
+multiplicationOfNumbers(9)
---
+++ sumOfNumbers.js
@@ -0,0 +1,21 @@
+// 1 + 2 + 3 + 4 + 6 + 7 + 8 + 9
+function sumOfNumbers(number) {
+    let = sum = 0 ;
+    for (let i = 1; i <= number; i++) {
+        sum = sum + i;
+        console.log(i, sum)
+    }
+    return sum ;
+}
+
+sumOfNumbers(9)
+
+
+// function sumofNumbers (number){
+//     let sum = 0;
+//     for (let i = 1; i <= number; i++) {
+//     sum = sum + i;
+//     console. log (i, sum);
+//     return sum
+//     }
+// }
