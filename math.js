41c85f552d393a591a5ecf836b7b708b99048087
Arman Shahariar <111569490+armanshahriar@users.noreply.github.com>
2023-04-08 02:40:43 +0600

    version - 1.1.6

---
+++ math.js
@@ -0,0 +1,13 @@
+const result = Math.pow(5,6)
+console.log(result)
+
+const num = 45;
+const num1 = 90;
+
+const gap =Math.abs(num - num1) ;
+if (gap < 5) {
+    console.log("ay bonshu kache ay")
+}
+else{
+    console.log("Amar chulkani ase")
+}
